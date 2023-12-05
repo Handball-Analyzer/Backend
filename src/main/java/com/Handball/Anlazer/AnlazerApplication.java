6219fb421d29ea89d6c1c87818cec5d5e18a17f1
mabu2807 <marcel_bulling@gmx.de>
2023-12-05 18:58:06 +0100

    init commit

---
+++ src/main/java/com/Handball/Anlazer/AnlazerApplication.java
@@ -0,0 +1,13 @@
+package com.Handball.Anlazer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnlazerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnlazerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Handball/Anlazer/AnlazerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Handball.Anlazer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnlazerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
